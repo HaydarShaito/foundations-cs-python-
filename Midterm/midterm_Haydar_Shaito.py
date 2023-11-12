93c4452b842e298d39d7fe8d4dc5caf4ca01e5fa
Haydar Jamal Shaito <148596354+HaydarShaito@users.noreply.github.com>
2023-11-12 16:35:44 +0200

    Update midterm_Haydar_Shaito.py

--- Midterm/midterm_Haydar_Shaito.py
+++ Midterm/midterm_Haydar_Shaito.py
@@ -1,3 +1,10 @@
+#1)
+def addNewTab(title,url):
+    lst.append({"Title",title})
+    lst.append({"URL",url})
+    #print(lst)
+
+#menu
 print("""
 1. Open Tab
 2. Close Tab
@@ -10,9 +17,16 @@ print("""
 9. Exit
 """)
 
+#main list
+lst=[]
+#choice loop
 choice=""
 while(choice!="9"):
     choice=input("Enter a command: ")
+    #make sure to enter a positive nb
     while(not choice.isnumeric()):
         choice=input("Please enter a positive integer: ")
-    
+    if(choice=="1"):
+        title=input("Enter the page title: ")
+        url=input("Enter the page URL: ")
+        addNewTab(title,url)
